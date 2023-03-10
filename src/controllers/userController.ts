7de59fb1d66b91e756868b657d86340a05556cfd
Rodinei Martins Coelho <rodinei.m.coelho@gmail.com>
2023-03-10 01:58:10 -0400

    refactor: move the signup to a service

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,38 +1,15 @@
 import { Request, Response } from 'express';
-import { Error } from 'mongoose';
-import validator from 'validator';
-import User, { IUser } from '../models/User';
+import { IUser } from '../models/User';
+import { signUpService } from '../services/userServices';
 import APIError from '../utils/APIError';
 
 export async function signUp(req: Request, res: Response): Promise<Response> {
   const user: IUser = req.body;
   const confirmPassword = req.body.confirmPassword || '';
-  const newUser = new User(user);
-  const errors: APIError[] = [];
-
-  if (validator.isEmpty(confirmPassword, { ignore_whitespace: true })) {
-    errors.push(
-      new APIError('confirmPassword', 'A user must confirm the password')
-    );
-  }
-
-  if (user.password !== confirmPassword) {
-    errors.push(new APIError('confirmPassword', 'Passwords are not the same'));
-  }
 
   try {
-    await newUser.save();
-  } catch (err: unknown) {
-    if (err instanceof Error.ValidationError) {
-      const error = err as Error.ValidationError;
-      errors.push(...APIError.fromValidationError(error));
-    } else {
-      const error = err as Error;
-      errors.push(new APIError(undefined, error.message));
-    }
-  }
+    const newUser = await signUpService(user, confirmPassword);
 
-  if (errors.length === 0) {
     newUser.password = '';
     return res.status(201).json({
       status: 'success',
@@ -40,10 +17,12 @@ export async function signUp(req: Request, res: Response): Promise<Response> {
         user: newUser
       }
     });
+  } catch (err: unknown) {
+    const errors =
+      err instanceof Array<APIError> ? err : 'Something went wrong';
+    return res.status(400).json({
+      status: 'fail',
+      errors
+    });
   }
-
-  return res.status(400).json({
-    status: 'fail',
-    errors
-  });
 }
