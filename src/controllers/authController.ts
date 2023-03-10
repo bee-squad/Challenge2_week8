c401723c712deaa51398418577274fe583ce1377
unknown <febomtempo@hotmail.com>
2023-03-10 18:16:19 -0300

    feat: auth middleware to protect routes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,17 @@
-import { Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { IUser } from '../models/User';
+import { Response, Request, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import User, { IUser } from '../models/User';
 import { Types } from 'mongoose';
 import dotenv from 'dotenv';
 import APIError from '../utils/APIError';
+
+export interface ReqWithUser extends Request {
+  user?: IUser;
+}
+
+export interface ReqWithToken extends Request {
+  token: string | JwtPayload;
+}
 
 dotenv.config({ path: '.env' });
 
@@ -35,3 +43,54 @@ export async function createSendToken(
     });
   }
 }
+
+// DOING...
+export const protect = async (
+  req: ReqWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return next(
+        res.status(401).json({
+          satatus: 'fail',
+          message: 'You are not logged in. Please log in to get access'
+        })
+      );
+    }
+
+    let decoded;
+    if (process.env.JWT_SECRET) {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+      (req as ReqWithToken).token = decoded;
+    }
+    let currentUser;
+    if (decoded)
+      currentUser = await User.findById((decoded as Types.ObjectId).id);
+    if (!currentUser) {
+      return next(
+        res.status(401).json({
+          satatus: 'fail',
+          message: 'The user belonging to this token does no longer exist.'
+        })
+      );
+    }
+    req.user = currentUser;
+    res.locals.user = currentUser;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      status: 'fail',
+      message: 'You need to be logged in to get access'
+    });
+  }
+};
