9535ecddcbeb7a7e93fce868637fa2d5165315e2
Rodinei Martins Coelho <rodinei.m.coelho@gmail.com>
2023-03-10 00:57:42 -0400

    feat: add better error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,33 +1,49 @@
 import { Request, Response } from 'express';
+import { Error } from 'mongoose';
 import validator from 'validator';
 import User, { IUser } from '../models/User';
+import APIError from '../utils/APIError';
 
-export async function signUp(req: Request, res: Response): Promise<void> {
+export async function signUp(req: Request, res: Response): Promise<Response> {
+  const user: IUser = req.body;
+  const confirmPassword = req.body.confirmPassword || '';
+  const newUser = new User(user);
+  const errors: APIError[] = [];
+
+  if (validator.isEmpty(confirmPassword, { ignore_whitespace: true })) {
+    errors.push(
+      new APIError('confirmPassword', 'A user must confirm the password')
+    );
+  }
+
+  if (user.password !== confirmPassword) {
+    errors.push(new APIError('confirmPassword', 'Passwords are not the same'));
+  }
+
   try {
-    const user: IUser = req.body;
-    const confirmPassword = req.body.confirmPassword || '';
-    const newUser = new User(user);
-
-    if (validator.isEmpty(confirmPassword, { ignore_whitespace: true }))
-      throw new Error('A user must confirm the password');
-
-    await newUser.validate();
-
-    if (user.password !== confirmPassword)
-      throw new Error('Passwords are not the same');
-
     await newUser.save();
+  } catch (err: unknown) {
+    if (err instanceof Error.ValidationError) {
+      const error = err as Error.ValidationError;
+      errors.push(...APIError.fromValidationError(error));
+    } else {
+      const error = err as Error;
+      errors.push(new APIError(undefined, error.message));
+    }
+  }
+
+  if (errors.length === 0) {
     newUser.password = '';
-    res.status(201).json({
+    return res.status(201).json({
       status: 'success',
       data: {
         user: newUser
       }
     });
-  } catch (err: unknown) {
-    res.status(400).json({
-      status: 'fail',
-      message: (err as Error).message
-    });
   }
+
+  return res.status(400).json({
+    status: 'fail',
+    errors
+  });
 }
