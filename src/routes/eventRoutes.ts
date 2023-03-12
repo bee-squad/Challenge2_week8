23ddb8924f7aa9172f0edc6d123c39d13772f78f
Aline Gabriel <122990449+alinegbrI@users.noreply.github.com>
2023-03-12 15:51:50 -0400

    get events by id route created

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
 import { protect } from '../controllers/authController';
-import { getAllEvents, createEvent } from '../controllers/eventController';
+import { getAllEvents, createEvent, getEventById } from '../controllers/eventController';
 
 const router: Router = express.Router();
 
 router.post('/', protect, createEvent);
 router.get('/', protect, getAllEvents);
+router.get('/:id', protect, getEventById);
 
 export default router;
