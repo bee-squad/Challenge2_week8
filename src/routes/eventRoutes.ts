6c736268d47599b247f173271b32d9204921ecbe
Michel Mazeto <michel.mazeto.pb@compasso.com.br>
2023-03-12 18:47:41 -0400

    fix: changed parameter from params.id to query

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,13 +1,13 @@
 import express, { Router } from 'express';
 import { protect } from '../controllers/authController';
-import { getAllEvents, createEvent, deleteEventByWeekday} from '../controllers/eventController';
+import { getAllEvents, createEvent, deleteEventByWeekday } from '../controllers/eventController';
 
 const router: Router = express.Router();
 
-router.post('/', protect, createEvent);
-
-router.get('/', protect, getAllEvents);
-
-router.delete('/:weekday', protect, deleteEventByWeekday);
+router
+  .route('/')
+  .get(protect, getAllEvents)
+  .post(protect, createEvent)
+  .delete(protect, deleteEventByWeekday);
 
 export default router;
