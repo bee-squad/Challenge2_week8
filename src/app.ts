8c08e81c3245ad8b8ce7fbf7f4e16cef6c75e45f
unknown <febomtempo@hotmail.com>
2023-03-12 11:06:33 -0300

    feat: add event routes to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import cors from 'cors';
 import connectMongo from './database/connect';
 import userRoutes from './routes/userRoutes';
+import eventRoutes from './routes/eventRoutes';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 connectMongo();
 
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/events', eventRoutes);
 
 app.get('/api/v1/', (req: Request, res: Response) => {
   res.status(200).json({
