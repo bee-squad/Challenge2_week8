261cc04f9ffc5b8b3384041ece1a2e2b80dcba35
unknown <febomtempo@hotmail.com>
2023-03-13 14:34:34 -0300

    feat: get events by id

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,149 +1,158 @@
-import { Request, Response } from 'express'
-import Event, { IEvent } from '../models/Event'
-import APIError from '../utils/APIError'
-import mongoose from 'mongoose'
+import { Request, Response } from 'express';
+import Event, { IEvent } from '../models/Event';
+import APIError from '../utils/APIError';
+import mongoose, { isValidObjectId } from 'mongoose';
 
-export async function getAllEvents(req: Request, res: Response) {
-    try {
-        const events = await Event.find()
-        res.status(200).json({
-            status: 'success',
-            results: events.length,
-            data: {
-                data: events
-            }
-        })
-    } catch (err: unknown) {
-        console.error(err)
-        const apiError = new APIError('Cannot find events', '404')
-        res.status(404).json(apiError)
-    }
-}
+export async function getEvents(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const { id, weekday } = req.query;
+    const weekdays = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
 
-export async function getEventsByWeekday(req: Request, res: Response) {
-    try {
-        const weekday = req.query.weekday as string;
-        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-        if (!weekdays.includes(weekday)) {
-            return res.status(400).json({
-                status: 'fail',
-                message: 'Invalid weekday',
-            });
-        }
-
-        const events = await Event.find({ 'dateTime.dayOfWeek': weekday });
-
-        if (events.length === 0) {
-            return res.status(404).json({
-                status: 'fail',
-                message: `No events found on ${weekday}`,
-            });
-        }
-
-        res.status(200).json({
-            status: 'success',
-            results: events.length,
-            data: {
-                data: events,
-            },
+    const events = await Event.find();
+    if (id) {
+      if (!isValidObjectId(id)) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'Invalid ID'
         });
-    } catch (err) {
-        if (err instanceof mongoose.Error.ValidationError) {
-            const errors = Object.values(err.errors).map((error) => error.message);
-            return res.status(400).json({
-                status: 'fail',
-                errors,
-            });
-        } else if (err instanceof APIError) {
-            return res.status(400).json({
-                status: 'fail',
-                errors: [err.message],
-            });
-        } else {
-            return res.status(500).json({
-                status: 'error',
-                message: 'Something went wrong',
-            });
-        }
+      }
+      const event = await Event.findById(id);
+      if (event) {
+        return res.status(200).json({
+          event
+        });
+      } else {
+        return res.status(404).json({
+          message: 'Fail to find event'
+        });
+      }
+    } else if (weekday) {
+      if (!weekdays.includes(weekday as string)) {
+        return res.status(400).json({
+          status: 'fail',
+          message: 'Invalid weekday'
+        });
+      }
+      const filteredEvents = events.filter(
+        (event) =>
+          event.dateTime.getDay() === weekdays.indexOf(weekday as string)
+      );
+
+      if (filteredEvents.length === 0) {
+        return res.status(404).json({
+          status: 'fail',
+          message: `No events found on ${weekday}`
+        });
+      }
+      return res.status(200).json({
+        filteredEvents
+      });
+    } else {
+      return res.status(200).json({
+        events
+      });
     }
+  } catch (err: unknown) {
+    console.error(err);
+    const apiError = new APIError('Cannot find events', '404');
+    return res.status(404).json(apiError);
+  }
 }
 
 export async function createEvent(req: Request, res: Response) {
-    try {
-        const { description, dateTime } = req.body;
+  try {
+    const { description, dateTime } = req.body;
 
-        const existingEvent = await Event.findOne({ description });
+    const existingEvent = await Event.findOne({ description });
 
-        if (existingEvent) {
-            return res.status(400).json({
-                status: 'fail',
-                message: 'There is already an event with the same description',
-            });
-        }
-
-        const newEvent: IEvent = {
-            description,
-            dateTime,
-            createdAt: new Date(),
-        };
-
-        const event = await Event.create(newEvent);
-
-        res.status(201).json({
-            status: 'success',
-            data: {
-                data: event,
-            },
-        });
-    } catch (err: unknown) {
-        if (err instanceof mongoose.Error.ValidationError) {
-            const errors = Object.values(err.errors).map((error) => error.message);
-            return res.status(400).json({
-                status: 'fail',
-                errors,
-            });
-        } else if (err instanceof APIError) {
-            return res.status(400).json({
-                status: 'fail',
-                errors: [err.message],
-            });
-        } else {
-            return res.status(500).json({
-                status: 'error',
-                message: 'Something went wrong',
-            });
-        }
+    if (existingEvent) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'There is already an event with the same description'
+      });
     }
+
+    const newEvent: IEvent = {
+      description,
+      dateTime,
+      createdAt: new Date()
+    };
+
+    const event = await Event.create(newEvent);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: event
+      }
+    });
+  } catch (err: unknown) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      const errors = Object.values(err.errors).map((error) => error.message);
+      return res.status(400).json({
+        status: 'fail',
+        errors
+      });
+    } else if (err instanceof APIError) {
+      return res.status(400).json({
+        status: 'fail',
+        errors: [err.message]
+      });
+    } else {
+      return res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong'
+      });
+    }
+  }
 }
 
 export async function deleteEventByWeekday(req: Request, res: Response) {
-    try {
-        const { weekday } = req.query;
-        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  try {
+    const { weekday } = req.query;
+    const weekdays = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
 
-        const events = await Event.find();
-        const filteredEvents = events.filter(
-            (event) => event.dateTime.getDay() === weekdays.indexOf(weekday as string)
-        );
+    const events = await Event.find();
+    const filteredEvents = events.filter(
+      (event) => event.dateTime.getDay() === weekdays.indexOf(weekday as string)
+    );
 
-        if (filteredEvents.length === 0) {
-            return res.status(404).json({
-                status: 'fail',
-                message: `No events found on ${weekday}`,
-            });
-        }
-
-        for (const event of filteredEvents) {
-            await Event.deleteOne({ _id: event._id });
-        }
-
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
-    } catch (err: unknown) {
-        const apiError = new APIError('Cannot find events', '404')
-        return res.status(400).json(apiError)
+    if (filteredEvents.length === 0) {
+      return res.status(404).json({
+        status: 'fail',
+        message: `No events found on ${weekday}`
+      });
     }
+
+    for (const event of filteredEvents) {
+      await Event.deleteOne({ _id: event._id });
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err: unknown) {
+    const apiError = new APIError('Cannot find events', '404');
+    return res.status(400).json(apiError);
+  }
 }
